4f681c89f65e1498ed85f57246e30ad99ed4e048
Harsh0726 <harshanineranjana@gmail.com>
2025-04-05 22:12:39 +0530

    list doen polices for each resourses

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,21 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use App\Models\Vehicle;
+use App\Models\Guest;
+use App\Models\TimeLog;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
+    protected $policies = [
+        User::class => 'App\Policies\UserPolicy',
+        Vehicle::class => 'App\Policies\VehiclePolicy',
+        Guest::class => 'App\Policies\GuestPolicy',
+        TimeLog::class => 'App\Policies\TimeLogPolicy',
+    ];
     /**
      * Register any application services.
      */
@@ -14,11 +25,19 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
+
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Gate::before(function ($user, $ability) {
+            if ($user->hasRole('super_admin')) {
+                return true;
+            }
+        });
+
     }
 }
